51959ba7819cb4e18705ae73010d4b948718218c
deepaknagar <nagar392@gmail.com>
2020-01-14 17:23:47 +0530

    End of section 6

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import "./index.css";
 import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
 import "semantic-ui-css/semantic.min.css";
+import {
+  BrowserRouter
+} from "react-router-dom";
 
 const rootEl = document.getElementById("root");
 let render = () => {
-  ReactDOM.render(<App />, rootEl);
+  ReactDOM.render( <
+    BrowserRouter >
+    <
+    App / >
+    <
+    /BrowserRouter>,
+    rootEl
+  );
 };
 if (module.hot) {
   module.hot.accept("./app/layout/App", () => {
@@ -15,7 +25,10 @@ if (module.hot) {
   });
 }
 render();
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render( <
+  BrowserRouter > < App / > < /BrowserRouter>
+
+  , document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
